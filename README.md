4d2d51136e72bf118d12a8870c3c4b348012fc51
Nathan Hammond <github.com@nathanhammond.com>
2022-02-10 02:50:51 +0800

    Clean up the build.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-#Radiation Info
+# Radiation Info
 Concept, research, development, and testing by [Wrench Labs](http://www.wrenchlabs.com): [Nathan Hammond](http://www.nathanhammond.com) and [Chris Granger](http://www.chris-granger.com).
 
 Design by [Lindsay Burtner](http://www.lindsayburtner.com).
 
 [Source Data](https://spreadsheets.google.com/ccc?key=0ApnSRONV3LTVdEpxelZFMlFoTHlkWEgtZ3NiMHN2RlE&hl=en)
 
-##Questions
+## Questions
 Get in touch: radiationinfo@radiationinfo.org
--- build.sh
+++ build.sh
@@ -1,54 +1,70 @@
 #!/bin/sh
 
-# Clear the way!
-rm -rf ~/sites/radiationinfo
-cp -r ~/repos/radiationinfo ~/sites/
-rm -rf ~/sites/radiationinfo/.git
-rm ~/sites/radiationinfo/build.sh
-rm ~/sites/radiationinfo/README.md
+rm -rf ./build
+mkdir ./build
+mkdir ./build/_js
+mkdir ./build/_css
+mkdir ./build/_img
 
-# Prep source data
-wget -O ~/sites/radiationinfo/_src/acute.tsv 'https://spreadsheets.google.com/fm?key=0ApnSRONV3LTVdEpxelZFMlFoTHlkWEgtZ3NiMHN2RlE&hl=en&fmcmd=23&gid=1'
-wget -O ~/sites/radiationinfo/_src/longterm.tsv 'https://spreadsheets.google.com/fm?key=0ApnSRONV3LTVdEpxelZFMlFoTHlkWEgtZ3NiMHN2RlE&hl=en&fmcmd=23&gid=0'
-wget -O ~/sites/radiationinfo/_src/limits.tsv 'https://spreadsheets.google.com/fm?key=0ApnSRONV3LTVdEpxelZFMlFoTHlkWEgtZ3NiMHN2RlE&hl=en&fmcmd=23&gid=2'
+# Raw data as JSON.
+curl --silent -L 'https://spreadsheets.google.com/fm?key=0ApnSRONV3LTVdEpxelZFMlFoTHlkWEgtZ3NiMHN2RlE&hl=en&fmcmd=23&gid=1' | jq -s --slurp --raw-input --raw-output --compact-output -j 'split("\r\n") | .[1:-1] | map(split("\t")) | map({"description": .[0], "type": .[1], "level": .[2], "unit": .[3], "source": .[4]})' > ./build/_js/acute.json
+curl --silent -L 'https://spreadsheets.google.com/fm?key=0ApnSRONV3LTVdEpxelZFMlFoTHlkWEgtZ3NiMHN2RlE&hl=en&fmcmd=23&gid=0' | jq -s --slurp --raw-input --raw-output --compact-output -j 'split("\r\n") | .[1:-1] | map(split("\t")) | map({"description": .[0], "type": .[1], "level": .[2], "unit": .[3], "source": .[4]})' > ./build/_js/longterm.json
+curl --silent -L 'https://spreadsheets.google.com/fm?key=0ApnSRONV3LTVdEpxelZFMlFoTHlkWEgtZ3NiMHN2RlE&hl=en&fmcmd=23&gid=2' | jq -s --slurp --raw-input --raw-output --compact-output -j 'split("\r\n") | .[1:-1] | map(split("\t")) | map({"description": .[0], "type": .[1], "level": .[2], "unit": .[3], "source": .[4]})' > ./build/_js/limits.json
 
-node ~/sites/radiationinfo/_src/tojson.js acute > ~/sites/radiationinfo/_src/acute.json
-node ~/sites/radiationinfo/_src/tojson.js longterm > ~/sites/radiationinfo/_src/longterm.json
-node ~/sites/radiationinfo/_src/tojson.js limits > ~/sites/radiationinfo/_src/limits.json
-cat ~/sites/radiationinfo/_src/*.json > ~/sites/radiationinfo/_js/data.js
+touch ./build/_js/data.js
+printf "radiation.acute = " >> ./build/_js/data.js
+cat ./build/_js/acute.json >> ./build/_js/data.js
+printf ";" >> ./build/_js/data.js
+printf "\nradiation.longterm = " >> ./build/_js/data.js
+cat ./build/_js/longterm.json >> ./build/_js/data.js
+printf ";" >> ./build/_js/data.js
+printf "\nradiation.limits = " >> ./build/_js/data.js
+cat ./build/_js/limits.json >> ./build/_js/data.js
+printf ";" >> ./build/_js/data.js
 
-rm -rf ~/sites/radiationinfo/_src
+wget --quiet https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar -O yuicompressor-2.4.8.jar
 
 # Handle CSS
-java -jar ~/devtools/yuicompressor-2.4.2.jar ~/sites/radiationinfo/_css/reset.css -o ~/sites/radiationinfo/_css/reset.css
-java -jar ~/devtools/yuicompressor-2.4.2.jar ~/sites/radiationinfo/_css/screen.css -o ~/sites/radiationinfo/_css/screen.css
-java -jar ~/devtools/yuicompressor-2.4.2.jar ~/sites/radiationinfo/_css/ie.css -o ~/sites/radiationinfo/_css/ie.css
-java -jar ~/devtools/yuicompressor-2.4.2.jar ~/sites/radiationinfo/_css/320.css -o ~/sites/radiationinfo/_css/320.css
-java -jar ~/devtools/yuicompressor-2.4.2.jar ~/sites/radiationinfo/_css/radiation.css -o ~/sites/radiationinfo/_css/radiation.css
-touch ~/sites/radiationinfo/_css/all.css
-cat ~/sites/radiationinfo/_css/reset.css ~/sites/radiationinfo/_css/screen.css > ~/sites/radiationinfo/_css/all.css
-rm ~/sites/radiationinfo/_css/reset.css ~/sites/radiationinfo/_css/screen.css
+java -jar ./yuicompressor-2.4.8.jar ./_css/reset.css -o ./build/_css/reset.css
+java -jar ./yuicompressor-2.4.8.jar ./_css/screen.css -o ./build/_css/screen.css
+java -jar ./yuicompressor-2.4.8.jar ./_css/ie.css -o ./build/_css/ie.css
+java -jar ./yuicompressor-2.4.8.jar ./_css/320.css -o ./build/_css/320.css
+java -jar ./yuicompressor-2.4.8.jar ./_css/radiation.css -o ./build/_css/radiation.css
+touch ./build/_css/all.css
+cat ./build/_css/reset.css ./build/_css/screen.css > ./build/_css/all.css
+cp ./_css/PIE.htc ./build/_css/
+
+# Handle Images
+cp ./_img/* ./build/_img/
+
+# Handle favicon
+cp ./favicon.ico ./build/
 
 # Handle JS
-java -jar ~/devtools/yuicompressor-2.4.2.jar ~/sites/radiationinfo/_js/data.js -o ~/sites/radiationinfo/_js/data.js
-java -jar ~/devtools/yuicompressor-2.4.2.jar ~/sites/radiationinfo/_js/usepie.js -o ~/sites/radiationinfo/_js/usepie.js
-java -jar ~/devtools/yuicompressor-2.4.2.jar ~/sites/radiationinfo/_js/site.js -o ~/sites/radiationinfo/_js/site.js
-cat ~/sites/radiationinfo/_js/site.js ~/sites/radiationinfo/_js/data.js > ~/sites/radiationinfo/_js/embed.js
+java -jar ./yuicompressor-2.4.8.jar ./build/_js/data.js -o ./build/_js/data.js
+java -jar ./yuicompressor-2.4.8.jar ./_js/usepie.js -o ./build/_js/usepie.js
+java -jar ./yuicompressor-2.4.8.jar ./_js/site.js -o ./build/_js/site.js
+touch ./build/_js/embed.js
+cat ./build/_js/site.js ./build/_js/data.js > ./build/_js/embed.js
+touch ./build/_js/all.js
+cat ./_js/jquery.min.js ./_js/jquery-ui.min.js ./build/_js/site.js ./build/_js/data.js > ./build/_js/all.js
+cp ./_js/PIE.js ./build/_js/
 
-# Set up hosts
+# Handle index.html
+cp ./index.html ./build/
 
 case $1 in
     local)
 		#### LOCAL SETUP ####
-		# {{   }} http://localhost/
-		perl -p -i -e 's/{{([\w]*)}}/http:\/\/localhost\//g' ~/sites/radiationinfo/index.html ~/sites/radiationinfo/_css/* ~/sites/radiationinfo/_js/*;
+		# {{   }} http://localhost:8000/
+		perl -p -i -e 's/{{([\w]*)}}/http:\/\/localhost:8000\//g' ./build/index.html ./build/_css/* ./build/_js/*;
 		
 		# Remove Google Analytics
-		touch ~/sites/radiationinfo/temp.html
-		awk '/DOCTYPE/,/<\!\-\-Begin Google Analytics\-\->/' ~/sites/radiationinfo/index.html >> ~/sites/radiationinfo/temp.html
-		awk '/<\!\-\-End Google Analytics\-\->/,/<\/html>/' ~/sites/radiationinfo/index.html >> ~/sites/radiationinfo/temp.html
-		rm ~/sites/radiationinfo/index.html
-		mv ~/sites/radiationinfo/temp.html ~/sites/radiationinfo/index.html
+		touch ./build/temp.html
+		awk '/DOCTYPE/,/<\!\-\-Begin Google Analytics\-\->/' ./build/index.html >> ./build/temp.html
+		awk '/<\!\-\-End Google Analytics\-\->/,/<\/html>/' ./build/index.html >> ./build/temp.html
+		rm ./build/index.html
+		mv ./build/temp.html ./build/index.html
         ;;
     remote)
 		#### REMOTE SETUP ####
@@ -58,8 +74,8 @@ case $1 in
 		# {{IMAGE}} http://cdn.radiationinfo.org/
 		# {{JS}} http://cdn.radiationinfo.org/
 
-		perl -p -i -e 's/{{HOST}}/http:\/\/www\.radiationinfo\.org\//g' ~/sites/radiationinfo/index.html ~/sites/radiationinfo/_css/* ~/sites/radiationinfo/_js/*;
-		perl -p -i -e 's/{{([\w]*)}}/http:\/\/cdn\.radiationinfo\.org\//g' ~/sites/radiationinfo/index.html ~/sites/radiationinfo/_css/* ~/sites/radiationinfo/_js/*;
+		perl -p -i -e 's/{{HOST}}/https:\/\/www\.radiationinfo\.org\//g' ./build/index.html ./build/_css/* ./build/_js/*;
+		perl -p -i -e 's/{{([\w]*)}}/https:\/\/cdn\.radiationinfo\.org\//g' ./build/index.html ./build/_css/* ./build/_js/*;
         ;;
     *)
         echo $0 "local | remote"
